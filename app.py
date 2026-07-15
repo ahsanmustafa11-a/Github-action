54c383213cca24eb92b9681507d020da00cc08b0
Ahsan Mustafa <139356111+ahsanmustafa11-a@users.noreply.github.com>
2026-07-16 00:44:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ num3 = 15
 #num2 = float(input("Enter second number: "))
 #num3 = float(input("Enter third number: "))
 
-else fi
+
 if (num1 >= num2) and (num1 >= num3):
    largest = num1
 elif (num2 >= num1) and (num2 >= num3):
